8a657c5cbe7969e2b5757cd83521d7d2b31d8038
2dogsandaguy <Neverclear@ymail.com>
2023-09-04 09:38:11 -0400

    added user start in js and added more boxs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const grid = document.querySelector(".grid");
 const boxWidth = 100
 const boxHeight = 20
+
+const userStart = [230, 10]
+let currentPosition = userStart
 //create block
 class Box {
   constructor(xAxis, yAxis) {
@@ -14,15 +17,21 @@ class Box {
 //all my boxes
 const boxes = [
     new Box(10, 270),
-/*     new Box(120, 270),
+    new Box(120, 270),
     new Box(230, 270),
     new Box(340, 270),
-    new Box(450, 270), */
-   /*  new Box(10, 270),
-    new Box(10, 270),
-    new Box(10, 270),
-    new Box(10, 270),
-     */
+    new Box(450, 270),
+    new Box(10, 240),
+    new Box(120, 240),
+    new Box(230, 240),
+    new Box(340, 240),
+    new Box(450, 240),
+    new Box(10, 210),
+    new Box(120, 210),
+    new Box(230, 210),
+    new Box(340, 210),
+    new Box(450, 210),
+
 ]
 
 console.log(boxes[0]);
@@ -39,3 +48,10 @@ function createBoxes() {
 }
 
 createBoxes();
+
+const user = document.createElement("user");
+user.classList.add("user");
+user.style.left = currentPosition[0] + "px";
+
+user.style.bottom = currentPosition[1] + "px";
+grid.appendChild(user);
